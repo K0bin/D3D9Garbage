b3e21e422b04db0b595fc27b9efc1c885e7acdd3
Robin Kertels <robin.kertels@gmail.com>
2024-07-30 10:36:04 +0200

    Add support for D3D9Ex

--- D3D9Garbage/D3D9Tester.cpp
+++ D3D9Garbage/D3D9Tester.cpp
@@ -11,15 +11,32 @@
 #include <array>
 
 const Test SELECTED_TEST = Test::StretchRect;
+const bool EX = true;
 
 D3D9Tester::D3D9Tester(HWND window) {
-	dxvk::Com<IDirect3D9> d3d9 = Direct3DCreate9(D3D_SDK_VERSION);
+	HRESULT res;
+	dxvk::Com<IDirect3D9Ex> d3d9Ex;
+	dxvk::Com<IDirect3D9> d3d9;
+	if (!EX) {
+		d3d9 = Direct3DCreate9(D3D_SDK_VERSION);
+	}
+	else {
+		res = Direct3DCreate9Ex(D3D_SDK_VERSION, &d3d9Ex);
+		d3d9 = dxvk::Com<IDirect3D9>(d3d9Ex.ptr());
+	}
+	dxvk::Com<IDirect3DDevice9Ex> deviceEx;
 	dxvk::Com<IDirect3DDevice9> device;
 
 	D3DPRESENT_PARAMETERS params = {
 		640, 480, D3DFMT_A8R8G8B8, 1, D3DMULTISAMPLE_NONE, 0, D3DSWAPEFFECT_DISCARD, window, true, true, D3DFMT_D24S8, 0, 0, D3DPRESENT_INTERVAL_DEFAULT
 	};
-	HRESULT res = d3d9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, nullptr, D3DCREATE_HARDWARE_VERTEXPROCESSING, &params, &device);
+	if (!EX) {
+		res = d3d9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, nullptr, D3DCREATE_HARDWARE_VERTEXPROCESSING, &params, &device);
+	}
+	else {
+		res = d3d9Ex->CreateDeviceEx(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, nullptr, D3DCREATE_HARDWARE_VERTEXPROCESSING, &params, nullptr, &deviceEx);
+		device = dxvk::Com<IDirect3DDevice9>(deviceEx.ptr());
+	}
 
 	/*D3DPRESENT_PARAMETERS params = {
 		640, 480, D3DFMT_A8R8G8B8, 0, D3DMULTISAMPLE_NONE, 0, D3DSWAPEFFECT_DISCARD, nullptr, true, true, D3DFMT_D24S8, 0, 0, D3DPRESENT_INTERVAL_DEFAULT
--- D3D9Garbage/StretchRectTest.cpp
+++ D3D9Garbage/StretchRectTest.cpp
@@ -3,10 +3,27 @@
 StretchRectTest::StretchRectTest(HWND window, dxvk::Com<IDirect3D9>&& d3d9, dxvk::Com<IDirect3DDevice9>&& device)
 	: D3D9Test(window, std::move(d3d9), std::move(device))
 {
+	IDirect3DDevice9Ex* deviceEx = static_cast<IDirect3DDevice9Ex*>(device.ptr());
+
 	HRESULT res;
 	res = device->CreateDepthStencilSurface(1280, 720, D3DFMT_D24X8, D3DMULTISAMPLE_NONE, 0, false, &src, nullptr);
 	res = device->CreateDepthStencilSurface(1280, 720, D3DFMT_D32F_LOCKABLE, D3DMULTISAMPLE_NONE, 0, true, &dst, nullptr); // Lockable + Discard fails
 	res = device->CreateDepthStencilSurface(1280, 720, D3DFMT_D24X8, D3DMULTISAMPLE_NONE, 0, true, &dst, nullptr);
+
+	res = d3d9->CheckDeviceFormat(0, D3DDEVTYPE_HAL, D3DFMT_X8R8G8B8, D3DUSAGE_DEPTHSTENCIL, D3DRTYPE_SURFACE, D3DFMT_D32_LOCKABLE);
+	res = d3d9->CheckDepthStencilMatch(0, D3DDEVTYPE_HAL, D3DFMT_D32_LOCKABLE, D3DFMT_X8R8G8B8, D3DFMT_D24X8);
+	res = deviceEx->CreateDepthStencilSurfaceEx(1280, 720, D3DFMT_D32F_LOCKABLE, D3DMULTISAMPLE_NONE, 0, false, &src, nullptr, 0);
+	res = deviceEx->CreateDepthStencilSurfaceEx(1280, 720, D3DFMT_D32_LOCKABLE, D3DMULTISAMPLE_NONE, 0, false, &src, nullptr, 0);
+	res = deviceEx->CreateDepthStencilSurfaceEx(1280, 720, D3DFMT_D32_LOCKABLE, D3DMULTISAMPLE_NONE, 0, false, &src, nullptr, D3DUSAGE_DEPTHSTENCIL);
+
+	D3DLOCKED_RECT locked;
+	res = src->LockRect(&locked, nullptr, 0);
+	res = src->UnlockRect();
+
+	dxvk::Com<IDirect3DSurface9> backbuffer;
+	res = device->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backbuffer);
+	res = backbuffer->LockRect(&locked, nullptr, 0);
+	res = backbuffer->UnlockRect();
 }
 
 void StretchRectTest::Render()
